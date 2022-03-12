d6628ec21adbf33e81c19440cdf6a621b4358c44
Aman Izardar <55098834+amanizardar@users.noreply.github.com>
2022-03-12 18:42:46 +0530

    Add files via upload

--- frontend.py
+++ frontend.py
@@ -1,12 +1,64 @@
+from distutils.command.config import config
 import requests
 import json
+import socket
 
 
 
 username="xyz"
 
+def handle_data_scientist():
+    pass
+
+def handle_app_developer():
+    pass
+
+def download_config():
+    s= socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+    s.connect(("127.0.0.1",1236))
+
+    with open("config.json") as f:
+        while 1:
+            data = s.recv(1024)
+
+            if not data:
+                break
+            f.write(data)
+    s.close()
+    print("Config_file",'successfully downloaded.')
+
 def do_operations():
     print("you are inside do_operation method")
+    print("Press 1 if you are Want to deploy your AI model")
+    print("Press 2 if you are Want to deploy your Application")
+    print("Press 3 To Download Platform Config File")
+    print("Press 4 to exit")
+
+    opt = int(input())
+
+    if(opt==1):
+    # Data_Scientist handle
+        handle_data_scientist()
+        
+
+    elif(opt==2):
+        # App developer
+        handle_app_developer()
+
+    elif(opt==3):
+        download_config()
+
+    elif(opt==4):
+        exit()
+    else:
+        print("Error Please Try again!")
+
+
+
+
+
+
+    
 
 def isvalid(username,password):
     to_send={}
