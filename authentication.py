f26b02b9b28b62c4ffa9e86cd85535c3436a7d27
Aman Izardar <amanizardar@gmail.com>
2022-03-13 02:37:59 +0530

    Database splitted

--- authentication.py
+++ authentication.py
@@ -11,14 +11,20 @@ login_manager = LoginManager()
 login_manager.init_app(app)
 
 
-class User(UserMixin, db.Model):
+class User_DS(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    password = db.Column(db.String(80), nullable=False)
+
+class User_AD(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
     password = db.Column(db.String(80), nullable=False)
 
 
 
-@app.route('/add_user', methods = ['GET', 'POST'])
+
+@app.route('/add_user_DS', methods = ['GET', 'POST'])
 def do_signup():
     if(request.method=='POST'):
         data=request.get_json()
@@ -26,22 +32,55 @@ def do_signup():
         password = data['password']
         print("username is ",username)
         print("Password is ",password)
-        check_user = User.query.filter_by(username=username).first()
+        check_user = User_DS.query.filter_by(username=username).first()
         if(check_user is not None):
             return "User already registered, please sign in"
         else:
-            user = User(username=username, password=password)
+            user = User_DS(username=username, password=password)
             db.session.add(user)
             db.session.commit()
             return "Registered Successfully"
 
-@app.route('/authen', methods = ['GET', 'POST'])
+@app.route('/authen_DS', methods = ['GET', 'POST'])
 def authen():
     if(request.method=='POST'):
         data=request.get_json()
         username = data['username']
         password = data['password']
-        check_user = User.query.filter_by(username=username).first()
+        check_user = User_DS.query.filter_by(username=username).first()
+        if(check_user is not None):
+            if(check_user.password == password):
+                login_user(check_user)
+                return "ok"
+            else:
+                return "Incorrect Password"
+        else:
+            return "No such User exists"
+
+@app.route('/add_user_AD', methods = ['GET', 'POST'])
+def do_signup():
+    if(request.method=='POST'):
+        data=request.get_json()
+        username = data['username']
+        password = data['password']
+        print("username is ",username)
+        print("Password is ",password)
+        check_user = User_AD.query.filter_by(username=username).first()
+        if(check_user is not None):
+            return "User already registered, please sign in"
+        else:
+            user = User_AD(username=username, password=password)
+            db.session.add(user)
+            db.session.commit()
+            return "Registered Successfully"
+
+@app.route('/authen_AD', methods = ['GET', 'POST'])
+def authen():
+    if(request.method=='POST'):
+        data=request.get_json()
+        username = data['username']
+        password = data['password']
+        check_user = User_AD.query.filter_by(username=username).first()
         if(check_user is not None):
             if(check_user.password == password):
                 login_user(check_user)
--- requestmanager.py
+++ requestmanager.py
@@ -1,27 +1,79 @@
 from flask import Flask, request, render_template
 from flask import Flask
 import requests
+import cgi, os
+import cgitb; cgitb.enable()
+from werkzeug.utils import secure_filename
+
 
 app = Flask(__name__)
 
-@app.route('/signup', methods = ['GET', 'POST'])
+
+@app.route('/signup_DS', methods = ['GET', 'POST'])
 def signin():
     data = request.get_json()
     print("data got = ",data)
-    response=requests.post('http://localhost:1235/add_user',json=data).content
+    response=requests.post('http://localhost:1235/add_user_DS',json=data).content
     return "ok"
 
 
-@app.route('/login', methods = ['GET', 'POST'])
+@app.route('/login_DS', methods = ['GET', 'POST'])
 def login():
     if(request.method=='POST'): 
         data = request.get_json()
-        response=requests.post('http://localhost:1235/authen',json=data).content.decode()
+        response=requests.post('http://localhost:1235/authen_DS',json=data).content.decode()
         if(response=="ok"):
             return "ok"
         else:
             return "Error"
 
+@app.route('/signup_AD', methods = ['GET', 'POST'])
+def signin():
+    data = request.get_json()
+    print("data got = ",data)
+    response=requests.post('http://localhost:1235/add_user_AD',json=data).content
+    return "ok"
+
+
+@app.route('/login_AD', methods = ['GET', 'POST'])
+def login():
+    if(request.method=='POST'): 
+        data = request.get_json()
+        response=requests.post('http://localhost:1235/authen_AD',json=data).content.decode()
+        if(response=="ok"):
+            return "ok"
+        else:
+            return "Error"
+
+@app.route('/Upload', methods = ['GET', 'POST'])
+def upload():
+    if(request.method=='POST'): 
+
+        # Send this request to Scheduler
+
+        
+
+        # Just maintaining a copy here.
+
+        # app.config['UPLOAD_FOLDER'] = "./Data/Model/"
+        f = request.files['filename']
+        print(f)
+        print(f.filename)
+        f.save(os.path.join("./Data/Model/", f.filename))
+        # f.save(secure_filename(f.filename))
+
+        
+        # form = cgi.FieldStorage()
+        # this_fileitem = form['filename']
+        # if f.filename:
+        #     fn = os.path.basename(f.filename)
+        #     open('./Data/Model/' + fn, 'w').write(f.file.read())
+        #     message = 'The file "' + fn + '" was uploaded successfully'
+        # else:
+        #     message = 'No file was uploaded'
+       
+        # print(message)
+        return "KK"
 
 
 
